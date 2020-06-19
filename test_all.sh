5be6696d9858f67faff8a68373f6f5e827604ee3
Mario de Frutos <mario@defrutos.org>
2020-06-19 17:19:51 +0200

    Fix testing script

--- test_all.sh
+++ test_all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # on macs, you may need to:
 # export GOBUILDFLAG=-ldflags -linkmode=external
@@ -6,25 +6,22 @@
 coveralls_testflags="-covermode=count -coverprofile=coverage.out"
 
 echo "Running unit tests"
-ginkgo -r -race -randomizeAllSpecs -keepGoing -- -test.run TestGorp
-
-echo "Testing against gomysql"
-export GORP_TEST_DSN="gorptest:gorptest@tcp(192.168.254.7:3306)/gorptest"
-export GORP_TEST_DIALECT=gomysql
-#go test $coveralls_testflags $GOBUILDFLAG $@ .
-gotestsum --format testname --no-summary=skipped,failed -- $coveralls_testflags $GOBUILDFLAG .
-
-echo "Testing against postgres"
-export GORP_TEST_DSN="host=192.168.254.5 user=gorptest password=gorptest dbname=gorptest sslmode=disable"
-export GORP_TEST_DIALECT=postgres
-#go test $coveralls_testflags $GOBUILDFLAG $@ .
-gotestsum --format testname --no-summary=skipped,failed -- $coveralls_testflags $GOBUILDFLAG .
+go test -race
 
 echo "Testing against mymysql"
-export GORP_TEST_DSN="tcp:192.168.254.7:3306*gorptest/gorptest/gorptest"
+export GORP_TEST_DSN="tcp:localhost:3306*gorptest/gorptest/gorptest"
 export GORP_TEST_DIALECT=mysql
-gotestsum --format testname --no-summary=skipped,failed -- $coveralls_testflags $GOBUILDFLAG .
-#go test $coveralls_testflags $GOBUILDFLAG -test.run=$@ .
+go test $coveralls_testflags $GOBUILDFLAG -test.run=$@ .
+
+echo "Testing against gomysql"
+export GORP_TEST_DSN="gorptest:gorptest@tcp(localhost:3306)/gorptest"
+export GORP_TEST_DIALECT=gomysql
+go test $coveralls_testflags $GOBUILDFLAG $@ .
+
+echo "Testing against postgres"
+export GORP_TEST_DSN="host=localhost user=gorptest password=gorptest dbname=gorptest sslmode=disable"
+export GORP_TEST_DIALECT=postgres
+go test $coveralls_testflags $GOBUILDFLAG $@ .
 
 echo "Testing against sqlite"
 export GORP_TEST_DSN=/tmp/gorptest.bin
@@ -33,7 +30,7 @@ go test $coveralls_testflags $GOBUILDFLAG $@ .
 rm -f /tmp/gorptest.bin
 
 case $(go version) in
-  *go1.13*)
+  *go1.4*)
     if [ "$(type -p goveralls)" != "" ]; then
 	  goveralls -covermode=count -coverprofile=coverage.out -service=travis-ci
     elif [ -x $HOME/gopath/bin/goveralls ]; then
@@ -41,4 +38,4 @@ case $(go version) in
     fi
   ;;
   *) ;;
-esac%
+esac
